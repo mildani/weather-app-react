3384635e631cb5b6a1cbb6321b866abffc99f8a7
mildani <60314861+mildani@users.noreply.github.com>
2020-02-22 12:36:42 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header>
           Weather App
       </header>
-      <footer><a href="https://github.com/mildani/weather-app-react" target="_blank" rel="noopener noreferrer">Open-sourced code</a> by mildani</footer>
-    </div>
+      <footer><a href="https://github.com/mildani/weather-app-react" target="_blank" rel="noopener noreferrer">Open-source code</a> by mildani</footer>
+    </div></div>
   );
 }
 
