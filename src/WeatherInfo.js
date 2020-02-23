80bae9ef9cf5e9b891806ebc5d47442c0be8a715
mildani <60314861+mildani@users.noreply.github.com>
2020-02-23 17:27:38 +0100

    Added unit conversion Fahrenheit/Celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import FormattedDate from "./FormattedDate"
+import WeatherTemperature from "./WeatherTemperature"
 
 export default function WeatherInfo(props) {
     return(
@@ -7,7 +8,7 @@ export default function WeatherInfo(props) {
 <div className="timestamp"><FormattedDate date={props.data.date} /></div>
 <div className="city">{props.data.city}</div>
 <div className="description">{props.data.description}</div>
-<img src={props.data.icon} alt={props.data.description} /> <span className="temperature">{Math.round(props.data.temperature)}ºC|F</span>
+<img src={props.data.icon} alt={props.data.description} /> <span><WeatherTemperature celsius={props.data.temperature} /></span>
 <ul>
     <li> Humidity: {props.data.humidity}%</li>
     <li> Wind: {props.data.wind} km/h</li>
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react"
+
+export default function WeatherTemperature(props) {
+    const [unit, setUnit] = useState("celsius")
+    
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit")
+    }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius")
+    }
+
+    function convertToFahrenheit() {
+        return (Math.round((props.celsius *9/5)+32))
+    }
+    
+    
+    if (unit==="celsius") {
+    return (
+        <span className="WeatherTemperature">
+            <span>{ Math.round(props.celsius)}</span>
+            <span className="units">ºC|<a href="/" onClick={showFahrenheit}>ºF</a></span>
+            </span>
+    )} else {
+        return (
+            <span className="WeatherTemperature">
+            <span>{ convertToFahrenheit()}</span>
+            <span className="units"><a href="/" onClick={showCelsius}>ºC</a>|ºF</span>
+            </span>
+        )
+    }
+}
