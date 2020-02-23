b6ea2afbf454a9d1bd470f44a7354a34d70bab50
mildani <60314861+mildani@users.noreply.github.com>
2020-02-23 19:12:28 +0100

    Formatted Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -32,33 +32,33 @@ function formatHours(date) {
     return (
     <div className="WeatherForecast"> 
     <div className="separator">Upcoming Forecast</div>
-    <div className="forecastTable">
     <div className="row">
-      <div className="col hours">{formatHours(new Date(forecast.list[0].dt*1000))}</div>
-      <div className="col"><img src={imgUrl(0)} alt={forecast.list[0].weather[0].description}/></div>
-      <div className="col temp">{Math.round(forecast.list[0].main.temp)}ºC</div>
+      <div className="col-1"></div>
+      <div className="col-2">
+      <div className="hours">{formatHours(new Date(forecast.list[0].dt*1000))}</div>
+      <div className="image"><img src={imgUrl(0)} alt={forecast.list[0].weather[0].description}/></div>
+      <div className="temp">{Math.round(forecast.list[0].main.temp)}ºC</div>
+      </div>
+    <div className="col-2">
+    <div className="hours">{formatHours(new Date(forecast.list[1].dt*1000))}</div>
+      <div className="image"><img src={imgUrl(1)} alt={forecast.list[1].weather[0].description}/></div>
+      <div className="temp">{Math.round(forecast.list[1].main.temp)}ºC</div>
     </div>
-    <div className="row">
-    <div className="col hours">{formatHours(new Date(forecast.list[1].dt*1000))}</div>
-      <div className="col"><img src={imgUrl(1)} alt={forecast.list[1].weather[0].description}/></div>
-      <div className="col temp">{Math.round(forecast.list[1].main.temp)}ºC</div>
+    <div className="col-2">
+    <div className="hours">{formatHours(new Date(forecast.list[2].dt*1000))}</div>
+      <div className="image"><img src={imgUrl(2)} alt={forecast.list[2].weather[0].description}/></div>
+      <div className="temp">{Math.round(forecast.list[2].main.temp)}ºC</div>
     </div>
-    <div className="row">
-    <div className="col hours">{formatHours(new Date(forecast.list[2].dt*1000))}</div>
-      <div className="col"><img src={imgUrl(2)} alt={forecast.list[2].weather[0].description}/></div>
-      <div className="col temp">{Math.round(forecast.list[2].main.temp)}ºC</div>
+    <div className="col-2">
+    <div className="hours">{formatHours(new Date(forecast.list[3].dt*1000))}</div>
+      <div className="image"><img src={imgUrl(3)} alt={forecast.list[3].weather[0].description}/></div>
+      <div className="temp">{Math.round(forecast.list[3].main.temp)}ºC</div>
     </div>
-    <div className="row">
-    <div className="col hours">{formatHours(new Date(forecast.list[3].dt*1000))}</div>
-      <div className="col"><img src={imgUrl(3)} alt={forecast.list[3].weather[0].description}/></div>
-      <div className="col temp">{Math.round(forecast.list[3].main.temp)}ºC</div>
-    </div>
-    <div className="row">
-    <div className="col hours">{formatHours(new Date(forecast.list[4].dt*1000))}</div>
-      <div className="col"><img src={imgUrl(4)} alt={forecast.list[4].weather[0].description}/></div>
-      <div className="col temp">{Math.round(forecast.list[4].main.temp)}ºC</div>
-    </div></div>
-    </div>)} else {
+    <div className="col-2">
+    <div className="hours">{formatHours(new Date(forecast.list[4].dt*1000))}</div>
+      <div className="image"><img src={imgUrl(4)} alt={forecast.list[4].weather[0].description}/></div>
+      <div className="temp">{Math.round(forecast.list[4].main.temp)}ºC</div>
+    </div> <div className="col-1"></div></div></div>)} else {
          const apiKey = "3a94f3778290bfeee61278505dbbe51d";
          let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
          axios.get(apiUrl).then(handleResponse);
